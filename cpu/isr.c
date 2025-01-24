f05beb116de8a79ac8ed0828c5cb58c8b95e33b0
yoavthewk <yoavarviv1@gmail.com>
2025-01-24 22:44:36 +0200

    added exception string array

--- cpu/isr.c
+++ cpu/isr.c
@@ -3,10 +3,45 @@
 #include <arch/x86/cpu/isr.h>
 #include <arch/x86/cpu/idt.h>
 
+static const char* exception_labels[] = {
+    "[0x00] Divide by Zero Exception",
+    "[0x01] Debug Exception",
+    "[0x02] Unhandled Non-maskable Interrupt",
+    "[0x03] Breakpoint Exception",
+    "[0x04] Overflow Exception",
+    "[0x05] Bound Range Exceeded Exception",
+    "[0x06] Invalid Opcode/Operand Exception",
+    "[0x07] Device Unavailable Exception",
+    "[0x08] Double Fault",
+    "[0x09] Coprocessor Segment Overrun",
+    "[0x0A] Invalid TSS Exception",
+    "[0x0B] Absent Segment Exception",
+    "[0x0C] Stack-segment Fault",
+    "[0x0D] General Protection Fault",
+    "[0x0E] Page Fault",
+    "[0x0F] Inexplicable Error",
+    "[0x10] x87 Floating Exception",
+    "[0x11] Alignment Check",
+    "[0x12] Machine Check",
+    "[0x13] SIMD Floating Exception",
+    "[0x14] Virtualized Exception",
+    "[0x15] Control Protection Exception",
+    "[0x16] Inexplicable Error",
+    "[0x17] Inexplicable Error",
+    "[0x18] Inexplicable Error",
+    "[0x19] Inexplicable Error",
+    "[0x1A] Inexplicable Error",
+    "[0x1B] Inexplicable Error",
+    "[0x1C] Hypervisor Intrusion Exception",
+    "[0x1D] VMM Communications Exception",
+    "[0x1E] Security Exception",
+    "[0x1F] Inexplicable Error"
+};
+
 void isr_handler(uint8_t exception_number) {
     char exception[3] = { 0 };
     itoa((uint32_t)exception_number, exception);
-    kprint("Got exception: ");
+    kprint("FATAL EXCEPTION: ");
     kprint(exception);
     kprint("\n");
     __asm__ volatile ("cli; hlt");
